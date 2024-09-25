ef126d2eec24b3034f8b002a4921aa445a93f520
kimjaeik95 <wodlrdlsla@naver.com>
2024-09-25 23:32:09 +0900

    first commit

---
+++ src/main/java/com/SecretsOfKitchen/SecretsOfKitchen/SecretsOfKitchenApplication.java
@@ -0,0 +1,13 @@
+package com.SecretsOfKitchen.SecretsOfKitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretsOfKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretsOfKitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SecretsOfKitchen/SecretsOfKitchen/SecretsOfKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SecretsOfKitchen.SecretsOfKitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretsOfKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
